fdc2523fe39312ed04e079aebd9594ec0b21fb2d
a8kj7sea <104179839+a8kj7sea@users.noreply.github.com>
2024-08-20 16:57:42 +0300

    Create FullConfig.java

---
+++ examples/src/test/FullConfig.java
@@ -0,0 +1,37 @@
+package me.a8kj.test;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import lombok.NonNull;
+import me.a8kj.configuration.impl.A8kjCfgImpl;
+import me.a8kj.configuration.parent.annotations.base.Settings;
+import me.a8kj.configuration.parent.annotations.base.Where;
+
+@Settings(
+    backupOnSave = true, 
+    where = @Where(
+        path = "configs/test",
+        custom = true,
+        backup = @Where.Backup(
+            path = "backups",
+            custom = true,
+            extension = ".bak"
+        )
+    )
+)
+public class ConfigTest extends A8kjCfgImpl {
+
+    public ConfigTest(@NonNull String fileName, JavaPlugin plugin) {
+        super(fileName, plugin);
+        this.create();
+    }
+
+    public void setString(String text) {
+        if (getFileConfiguration() == null) {
+            throw new IllegalStateException("FileConfiguration is null!");
+        }
+
+        this.getFileConfiguration().set("Test", text);
+        this.save();
+    }
+}
